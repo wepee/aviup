6bce88fc514a59695f73703b87179d835b7ebeaa
wepee <adam59140@gmail.com>
2021-09-06 03:27:55 +0200

    add tableau mode

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="parent-frame">
-    <iframe src="https://sharing.clickup.com/l/h/6-128546959-1/b214b8d06b3566b" />
+    <div style="position: absolute; align-items: center; display: flex; top: -10px; left: calc(50% - 60px); z-index: 100000">
+      <v-icon>mdi-table</v-icon>
+      <h4 class="px-2">
+        Tableau
+      </h4>
+      <v-switch v-model="tableau" style="display: inline-block" />
+    </div>
+    <iframe :src="tableau ? 'https://sharing.clickup.com/b/h/6-128546959-2/2970c84cf5222bf' : 'https://sharing.clickup.com/l/h/6-128546959-1/b214b8d06b3566b'" />
   </div>
 </template>
 
@@ -8,7 +15,7 @@
 export default {
   data () {
     return {
-      list: null
+      tableau: false
     }
   }
 }
